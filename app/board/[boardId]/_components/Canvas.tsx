f32641841d140c2eb1d0a386c2872f6bfc84e2e3
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2024-02-06 15:26:11 -0300

    Liveblock room authentication

--- app/board/[boardId]/_components/Canvas.tsx
+++ app/board/[boardId]/_components/Canvas.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import { useSelf } from "@/liveblocks.config";
+
 import Info from "./Info";
-import Participants from "./Participants";
 import Toolbar from "./Toolbar";
+import Participants from "./Participants";
 
 interface CanvasProps {
   boardId: string;
 }
 
 function Canvas({ boardId }: CanvasProps) {
+  const { name, picture } = useSelf((me) => me.info);
+  console.log("name", name);
+  console.log("picture", picture);
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info />
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,8 +2,7 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 
 const client = createClient({
-  publicApiKey:
-    "pk_dev_V3rGOWvDjJax3BZkg95WXoBUABRksynLojqqDTxTMbhmDMjeiLh_C-xoDpASugBM",
+  authEndpoint: "/api/liveblocks-auth",
   // authEndpoint: "/api/auth",
   // throttle: 100,
 });
@@ -29,8 +28,11 @@ type Storage = {
 // provided by your own custom auth back end (if used). Useful for data that
 // will not change during a session, like a user's name or avatar.
 type UserMeta = {
-  // id?: string,  // Accessible through `user.id`
-  // info?: Json,  // Accessible through `user.info`
+  id?: string;
+  info: {
+    name?: string;
+    picture?: string;
+  };
 };
 
 // Optionally, the type of custom events broadcast and listened to in this
