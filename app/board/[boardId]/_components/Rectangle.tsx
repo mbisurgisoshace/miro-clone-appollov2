c4424a66c6ec0d9b42091c9b6a9c1f9e9697dfd4
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2024-03-25 11:37:04 -0300

    Grouping

--- app/board/[boardId]/_components/Rectangle.tsx
+++ app/board/[boardId]/_components/Rectangle.tsx
@@ -16,6 +16,24 @@ function Rectangle({
 }: RectangleProps) {
   const { x, y, width, height, fill } = layer;
   return (
+    // <g
+    //   x={0}
+    //   y={0}
+    //   width={width}
+    //   height={height}
+    //   strokeWidth={1}
+    //   className="drop-shadow-md"
+    //   fill={fill ? colorToCss(fill) : "#000"}
+    //   stroke={selectingColor || "transparent"}
+    //   style={{
+    //     transform: `translate(${x}px, ${y}px)`,
+    //     backgroundColor: "white",
+    //   }}
+    //   onPointerDown={(e) => onPointerDown(e, id)}
+    // >
+    //   <rect x={0} y={0} width={25} height={25} />
+    //   <rect x={50} y={50} width={25} height={25} />
+    // </g>
     <rect
       x={0}
       y={0}
